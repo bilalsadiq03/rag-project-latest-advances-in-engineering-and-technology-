961a36bbd4f5486845c4f87a94567c802b4237b7
Bilal Sadiq <bnaq860@gmail.com>
2026-03-26 14:22:18 +0530

    add basic project setup

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from routes.upload import router as upload_router
+
+app = FastAPI()
+
+app.include_router(upload_router)
+
+@app.get("/")
+def home():
+    return {
+        "status": "ok"
+    }
